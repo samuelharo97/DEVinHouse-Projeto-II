86f2cb00b9ef9b61ab6cbe07d43e6f574dc368c9
Samuel Haro <samuel.hm@hotmail.com>
2022-09-09 23:33:24 -0300

    commit #2 add: theme

---
+++ src/styles/theme.js
@@ -0,0 +1,42 @@
+export const theme = {
+  COLORS: {
+    PRIMARY: {
+      LIGHT: '#F0B27A',
+      MAIN: '#FF8818',
+      DARK: '#CA6F1E'
+    },
+
+    SECONDARY: {
+      LIGHT: '#5D6D7E',
+      MAIN: '#2E4052',
+      DARK: '#212F3C'
+    },
+
+    ERROR: {
+      LIGHT: '#E57373',
+      MAIN: '#F44336',
+      DARK: '#D32F2F'
+    },
+    WARNING: {
+      LIGHT: '#FFB74D',
+      MAIN: '#FFA726',
+      DARK: '#F57C00'
+    },
+    INFO: {
+      LIGHT: '#4FC3F7',
+      MAIN: '#29B6F6',
+      DARK: '#0288D1'
+    },
+    SUCCESS: {
+      LIGHT: '#81C784',
+      MAIN: '#66BB6A',
+      DARK: '#388E3C'
+    },
+    COMMON: {
+      GRAY: '#388E3C',
+      GRAY_TEXT: '#388E3C',
+      WHITE: '#FFFFFF',
+      BLACK: '#000000'
+    }
+  }
+}
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
+
 import eslint from 'vite-plugin-eslint'
+
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@styles': path.resolve(__dirname, './src/styles/index.js'),
+      '@components': path.resolve(__dirname, './src/components/index.js'),
+      '@context': path.resolve(__dirname, './src/context/index.js'),
+      '@services': path.resolve(__dirname, './src/services/index.js'),
+      '@hooks': path.resolve(__dirname, './src/hooks/index.js'),
+      '@pages': path.resolve(__dirname, './src/pages/index.js'),
+      '@router': path.resolve(__dirname, './src/router/index.js')
+    }
+  },
   plugins: [react(), eslint()]
 })
